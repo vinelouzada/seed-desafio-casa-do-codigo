1a8fb8d7d99b91fdf2c0523249dedb6e91b5c75f
Vinicius Louzada <vinelouzada@gmail.com>
2025-03-03 08:57:54 -0300

    feat: document validation with constraint composition

--- src/main/java/vinelouzada/cdc/shared/Document.java
+++ src/main/java/vinelouzada/cdc/shared/Document.java
@@ -2,15 +2,23 @@ package vinelouzada.cdc.shared;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+import org.hibernate.validator.constraints.CompositionType;
+import org.hibernate.validator.constraints.ConstraintComposition;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = {DocumentValidator.class})
-@Target({ElementType.FIELD})
+import static java.lang.annotation.ElementType.*;
+
+@Target({FIELD, METHOD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@ConstraintComposition(CompositionType.OR)
+@CPF
+@CNPJ
 public @interface Document {
     String message() default "The document format is not supported";
     Class<?>[] groups() default {};
